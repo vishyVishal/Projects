3af850376f7eab24215f675ed279828306f143b9
Vishal <vishal.singh.sh@gmail.com>
2021-06-02 21:58:19 +0800

    Update: Added 'if_exists='replace'' in df.to_sql

--- disaster_response/data/process_data.py
+++ disaster_response/data/process_data.py
@@ -78,7 +78,7 @@ def save_data(df, database_filename):
     """
     engine = create_engine('sqlite:///' + database_filename)
     
-    df.to_sql('CorrectedTable', engine, index=False)
+    df.to_sql('CorrectedTable', engine, if_exists='replace', index=False)
 
 def main():
     if len(sys.argv) == 4:
